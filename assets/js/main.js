b11a07c8b7a98612269d0934c2555062409fae73
ORxanone <recebovorxan3@gmail.com>
2022-01-05 22:30:18 -0800

    website

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+$(".img1").hover(function() {
+    document.getElementById("box_img").src = "assets/img/pr-12.jpg";
+    document.getElementById("box_img").style.transform = "scale(1.2)";
+}, function() {
+    document.getElementById("box_img").src = "assets/img/pr-11.jpg";
+    document.getElementById("box_img").style.transform = "scale(1)";
+});
+
+$(".img2").hover(function() {
+    document.getElementById("box_img2").src = "assets/img/pr-12.jpg";
+    document.getElementById("box_img2").style.transform = "scale(1.2)";
+}, function() {
+    document.getElementById("box_img2").src = "assets/img/pr-11.jpg";
+    document.getElementById("box_img2").style.transform = "scale(1)";
+});
+$(".img3").hover(function() {
+    document.getElementById("box_img3").src = "assets/img/pr-12.jpg";
+    7
+    document.getElementById("box_img3").style.transform = "scale(1.2)";
+}, function() {
+    document.getElementById("box_img3").src = "assets/img/pr-11.jpg";
+    document.getElementById("box_img3").style.transform = "scale(1)";
+});
+$(".img4").hover(function() {
+    document.getElementById("box_img4").src = "assets/img/pr-12.jpg";
+    document.getElementById("box_img4").style.transform = "scale(1.2)";
+}, function() {
+    document.getElementById("box_img4").src = "assets/img/pr-11.jpg";
+    document.getElementById("box_img4").style.transform = "scale(1)";
+});
+
+
+
+
+
+$(".recently_product .imgg1").hover(function() {
+    document.getElementById("rp_box_img").src = "assets/img/pr-04.jpg";
+    document.getElementById("rp_box_img").style.transform = "scale(1.2)";
+
+}, function() {
+    document.getElementById("rp_box_img").src = "assets/img/pr-03.jpg";
+    document.getElementById("rp_box_img").style.transform = "scale(1)";
+});
+
+$(".recently_product .imgg2").hover(function() {
+    document.getElementById("rp_box_img2").src = "assets/img/pr-04.jpg";
+    document.getElementById("rp_box_img2").style.transform = "scale(1.2)";
+}, function() {
+    document.getElementById("rp_box_img2").src = "assets/img/pr-03.jpg";
+    document.getElementById("rp_box_img2").style.transform = "scale(1)";
+});
+$(".recently_product .imgg3").hover(function() {
+    document.getElementById("rp_box_img3").src = "assets/img/pr-04.jpg";
+    document.getElementById("rp_box_img3").style.transform = "scale(1.2)";
+}, function() {
+    document.getElementById("rp_box_img3").src = "assets/img/pr-03.jpg";
+    document.getElementById("rp_box_img3").style.transform = "scale(1)";
+});
+$(".recently_product .imgg4").hover(function() {
+    document.getElementById("rp_box_img4").src = "assets/img/pr-04.jpg";
+    document.getElementById("rp_box_img4").style.transform = "scale(1.2)";
+}, function() {
+    document.getElementById("rp_box_img4").src = "assets/img/pr-03.jpg";
+    document.getElementById("rp_box_img4").style.transform = "scale(1)";
+});
